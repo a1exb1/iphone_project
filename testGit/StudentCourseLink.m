b818b8fdb16f0fa2bcdb570f8688391e39f20bc4
Alex Bechmann <alexbechmann@Alexs-Mac.local>
2014-04-26 13:54:38 +0200

    studentcourselink class

---
+++ testGit/StudentCourseLink.m
@@ -0,0 +1,13 @@
+//
+//  StudentCourseLink.m
+//  testGit
+//
+//  Created by Alex Bechmann on 26/04/14.
+//  Copyright (c) 2014 Alex Bechmann. All rights reserved.
+//
+
+#import "StudentCourseLink.h"
+
+@implementation StudentCourseLink
+
+@end
